cb81f68f9a6c1fed47a2e3ea9a9a4b79cbbcc194
PatrykBaranski <pbaranski740@gmail.com>
2022-12-09 11:30:37 +0100

    adding clear button for search bar

--- src/components/header/SearchBar.jsx
+++ src/components/header/SearchBar.jsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { FaSearch } from "react-icons/fa";
 import { IconContext } from "react-icons";
 import { useSelector } from "react-redux";
 import classes from "./SearchBar.module.css";
 import { setSearchQuery } from "../../store";
+import { useEffect } from "react";
 
 const SearchBar = ({ onMouseEnter, onClick }) => {
+  const [isClearBtnVisible, setIsClearBtnVisible] = useState(false);
   const query = useSelector((state) => state.query);
   const dispatch = useDispatch();
+  useEffect(() => {
+    if (query.length >= 3) {
+      setIsClearBtnVisible(true);
+    } else setIsClearBtnVisible(false);
+  }, [query]);
   const subimitHandler = (e) => {
     e.preventDefault();
   };
   const changeInputHandler = (e) => {
     dispatch(setSearchQuery(e.target.value));
+  };
+  const clearHandler = () => {
+    dispatch(setSearchQuery(""));
   };
   return (
     <li
@@ -27,7 +38,12 @@ const SearchBar = ({ onMouseEnter, onClick }) => {
           placeholder="Search for a figure"
           value={query}
         />
-        <button type="submit">
+        {isClearBtnVisible && (
+          <button onClick={clearHandler} className={classes["btn--clear"]}>
+            CLEAR
+          </button>
+        )}
+        <button className={classes["btn--submit"]} type="submit">
           <IconContext.Provider value={{ color: "#333" }}>
             <FaSearch />
           </IconContext.Provider>
